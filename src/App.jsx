98fd0e94401c254702bb1b0f01a8937ee21c06ee
Muhammad Denni Andrian <105216050+mdenniandrian@users.noreply.github.com>
2026-05-06 23:03:12 +0700

    Change title

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ export default function ShiftApp() {
         
         {/* Header & Controls */}
         <div className="bg-gray-800 text-white p-6">
-          <h1 className="text-2xl md:text-3xl font-bold mb-4">Aplikasi Jadwal Shift Tahunan</h1>
+          <h1 className="text-2xl md:text-3xl font-bold mb-4">Aplikasi Jadwal Shift CS Maxcloud 2026</h1>
           
           <div className="flex flex-col sm:flex-row gap-4 items-center">
             <div className="flex items-center gap-2">
